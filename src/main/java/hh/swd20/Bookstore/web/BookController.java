09bd8ab2752c4bb6311ab79e58a594a6e10ebd83
Emma Haverinen <emma.haverinen@gmail.com>
2019-03-12 22:24:10 +0200

    security

--- src/main/java/hh/swd20/Bookstore/web/BookController.java
+++ src/main/java/hh/swd20/Bookstore/web/BookController.java
@@ -23,6 +23,11 @@ public class BookController {
 	@Autowired
 	private GenreRepository grepository; 
 	
+	@RequestMapping(value="/login")
+    public String login() {	
+        return "login";
+    }	
+	
     @RequestMapping(value="/booklist")
     public String bookList(Model model) {	
     	model.addAttribute("books", repository.findAll());
